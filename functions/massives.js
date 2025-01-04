38f431644de192e4ef751adf047ef3e932d902d6
suniaikin <class.suniaikin@gmail.com>
2025-01-03 16:53:46 -0800

    massives

---
+++ functions/massives.js
@@ -0,0 +1,20 @@
+// 0 => 60
+// 1 => 50
+// 2 => 60
+// 4 => 58
+// 5 => 54
+// 6 => 58
+// 7 => 50
+// 8 => 52
+// 9 => 54
+
+const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54];
+console.log(scores [0]);
+console.log(scores [5]);
+
+const pets = ["dog", "cat", "rat"]
+console.log(pets [0]);
+console.log(pets [2]);
+
+console.log(pets.length);
+
